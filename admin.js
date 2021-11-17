67503dff32910aefe0cfbc4d6fee4f93834131d8
DoQuocBinh <binhdq@fpt.edu.vn>
2021-11-17 10:21:56 +0700

    first commit

---
+++ admin.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.render('adminIndex')
+})
+
+router.get('/addStaff',(req,res)=>{
+    res.render('addStaff')
+})
+
+module.exports = router;
+
